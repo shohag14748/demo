1376df2dd0c6efcd227dd35d6d8c02ac2c12d6d9
Mohammad Ali Shohag <47399558+shohag14748@users.noreply.github.com>
2020-05-07 04:40:38 +0600

    upload
    upload

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        var wScroll = $(this).scrollTop();
+        $('.bg-dots').css({
+            'transform': 'translate(-47%,' + wScroll/40 + '%)'
+        });
+        if(wScroll>150){
+            $('.to-top').fadeIn();
+        }else{
+            $('.to-top').fadeOut();
+        }
+
+    });
+    $('.to-top').click(function(){
+        $('body,html').animate({
+            scrollTop: 0
+        }, 1000);
+    });
+
+//    var scrollLink = $('.smooth');
+//    scrollLink.on("click", function (e) {
+//        e.preventDefault();
+//        $('body,html').animate({
+//            scrollTop: $(this.hash).offset().top
+//        }, 1000);
+//    });
+
+});
